5ed96ea0c5334829466dc71e946f60ca327d430d
Jon Duckworth <duckontheweb@gmail.com>
2021-03-19 15:06:29 -0400

    Move Provider to separate module

--- rustac-core/stac-types/src/lib.rs
+++ rustac-core/stac-types/src/lib.rs
@@ -5,7 +5,8 @@ extern crate serde_json;
 pub use link::Link;
 pub use item::Item;
 pub use asset::Asset;
-pub use collection::{Collection, Extent, SpatialExtent, TemporalExtent, Provider};
+pub use collection::{Collection, Extent, SpatialExtent, TemporalExtent};
+pub use provider::Provider;
 pub use catalog::Catalog;
 pub use item_collection::ItemCollection;
 
@@ -16,3 +17,4 @@ pub mod asset;
 pub mod collection;
 pub mod catalog;
 pub mod item_collection;
+pub mod provider;
