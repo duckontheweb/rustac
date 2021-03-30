dd4bc1c1b7cbaa70f7061e7788babf13de9dd750
Jon Duckworth <duckontheweb@gmail.com>
2021-03-30 16:48:01 -0400

    Remove derived Validation trait and focus on validation functions

--- rustac-core/stac-validate/src/lib.rs
+++ rustac-core/stac-validate/src/lib.rs
@@ -1,6 +1,5 @@
 #![warn(missing_docs)]
 #![deny(clippy::all, clippy::pedantic)]
-#![allow(clippy::missing_errors_doc)]
 //! Tools for validating STAC objects
 
 extern crate semver;
@@ -8,215 +7,180 @@ extern crate serde_json;
 extern crate reqwest;
 extern crate jsonschema;
 
-use semver::Version;
-use serde::{Serialize};
-use serde_json::Value;
-use reqwest::blocking::get;
-use std::str::FromStr;
-use jsonschema::{JSONSchema, Draft};
+// use semver::Version;
+// use serde::{Serialize};
+// use serde_json::Value;
+// use reqwest::blocking::get;
+// use std::str::FromStr;
+// use jsonschema::{JSONSchema, Draft};
 
-pub use error::{Result, Error};
-use util::{root_url_from_version, path_from_stac_type};
+pub use util::{get_schema_url, get_schema};
+pub use validation::{is_valid_for_schema_type};
 
 mod util;
+mod validation;
 pub mod error;
 
-/// Possible types of schemas
-pub enum SchemaType {
-    /// Core JSON schemas
-    Core,
-
-    /// Electro-Optical extension schemas
-    EOExtension,
-
-    /// Projection extension schemas
-    ProjectionExtension,
-
-    /// Scientific Extension schemas
-    ScientificExtension,
-
-    /// View extension schemas
-    ViewExtension,
-}
-
-
-
-/// Adds functionality for getting JSON schemas and validating against them.
-pub trait Validate: Serialize {
-    /// Gets the type of the STAC object (e.g. Item)
-    fn get_type(&self) -> &String;
-
-    /// Gets the STAC version for this object.
-    fn get_stac_version(&self) -> &Version;
-
-    /// Gets the schema for this object
-    fn get_schema(&self, schema_type: SchemaType) -> Result<Value> {
-        let schema_url = match self.get_schema_url(schema_type) {
-            Some(url) => url,
-            None => { return Err(Error::from_str("Could not find schema.").unwrap())}
-        };
-
-        Ok(get(schema_url)?.json()?)
-    }
-
-    /// Gets the URL for the published JSON schema corresponding to the given object and schema type
-    /// (e.g. core, EO extension, etc.)
-    fn get_schema_url(&self, schema_type: SchemaType) -> Option<String>
-    {
-        // let stac_version = self.get_stac_version();
-        let root_url = root_url_from_version(self.get_stac_version());
-        let path = path_from_stac_type(&self.get_type(), &schema_type);
-        path.map(|path| format!("{}/{}", root_url.as_str(), path.as_str()))
-    }
-
-    /// Checks if the instance if valid when checked against the given schema.
-    fn is_valid_for_schema(&self, schema_type: SchemaType) -> Result<bool>
-    {
-        let schema = self.get_schema(schema_type)?;
-        Ok(JSONSchema::options()
-            .with_draft(Draft::Draft7)
-            .compile(&schema)?
-            .is_valid(&serde_json::to_value(self)?))
-
-    }
-}
-
-#[cfg(feature = "derive")]
-extern crate stac_validate_derive;
-#[cfg(feature = "derive")]
-pub use stac_validate_derive::Validation;
+// /// Adds functionality for getting JSON schemas and validating against them.
+// pub trait Validate: Serialize {
+//     /// Gets the type of the STAC object (e.g. Item)
+//     fn get_type(&self) -> &String;
+//
+//     /// Gets the STAC version for this object.
+//     fn get_stac_version(&self) -> &Version;
+//
+//     /// Gets the schema for this object
+//     fn get_schema(&self, schema_type: &str) -> Result<Value> {
+//         let schema_url = match self.get_schema_url(schema_type) {
+//             Some(url) => url,
+//             None => { return Err(Error::from_str("Could not find schema.").unwrap())}
+//         };
+//
+//         Ok(get(schema_url)?.json()?)
+//     }
+//
+//     /// Gets the URL for the published JSON schema corresponding to the given object and schema type
+//     /// (e.g. core, EO extension, etc.)
+//     fn get_schema_url(&self, schema_type: &str) -> Option<String>
+//     {
+//         let root_url = root_url_from_version(self.get_stac_version());
+//         let path = path_from_stac_type(&self.get_type(), schema_type);
+//         path.map(|path| format!("{}/{}", root_url.as_str(), path.as_str()))
+//     }
+//
+//     /// Checks if the instance if valid when checked against the given schema.
+//     fn is_valid_for_schema(&self, schema_type: &str) -> Result<bool>
+//     {
+//         let schema = self.get_schema(schema_type)?;
+//         Ok(JSONSchema::options()
+//             .with_draft(Draft::Draft7)
+//             .compile(&schema)?
+//             .is_valid(&serde_json::to_value(self)?))
+//
+//     }
+// }
 
 
-#[cfg(test)]
-mod tests {
-    use semver::Version;
-    use super::*;
-
-    #[derive(Serialize)]
-    struct STACObject {
-        stac_version: Version,
-        #[serde(rename = "type")]
-        type_: String
-    }
-    impl STACObject {
-        pub fn new(stac_version: &str, type_: &str) -> STACObject {
-            let stac_version = Version::parse(stac_version).unwrap();
-            let type_ = String::from(type_);
-            STACObject {
-                stac_version,
-                type_
-            }
-        }
-    }
-    impl Validate for STACObject {
-        fn get_type(&self) -> &String {
-            &self.type_
-        }
-        fn get_stac_version(&self) -> &Version { &self.stac_version }
-    }
-
-    #[test]
-    fn test_item_schemas_urls() {
-        // v1.0.0-rc.1 Item
-        let obj = STACObject::new("1.0.0-rc.1", "Feature");
-
-        // Core
-        assert_eq!(
-            obj.get_schema_url(SchemaType::Core).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/item-spec/json-schema/item.json"),
-        );
-
-        // EO Extension
-        assert_eq!(
-            obj.get_schema_url(SchemaType::EOExtension).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/eo/json-schema/schema.json"),
-        );
-
-        // Projection Extension
-        assert_eq!(
-            obj.get_schema_url(SchemaType::ProjectionExtension).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/projection/json-schema/schema.json"),
-        );
-
-        // Scientific Extension
-        assert_eq!(
-            obj.get_schema_url(SchemaType::ScientificExtension).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/scientific/json-schema/schema.json"),
-        );
-
-        // View Extension
-        assert_eq!(
-            obj.get_schema_url(SchemaType::ViewExtension).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/view/json-schema/schema.json"),
-        );
-    }
-
-    #[test]
-    fn test_collection_schema_urls() {
-        // v1.0.0-rc.1 Collection
-        let obj = STACObject::new("1.0.0-rc.1", "Collection");
-
-        // Core
-        assert_eq!(
-            obj.get_schema_url(SchemaType::Core).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/collection-spec/json-schema/collection.json"),
-        );
-
-        // EO Extension
-        assert!(obj.get_schema_url(SchemaType::EOExtension).is_none());
-
-        // Projection Extension
-        assert!(obj.get_schema_url(SchemaType::ProjectionExtension).is_none());
-
-        // Scientific Extension
-        assert_eq!(
-            obj.get_schema_url(SchemaType::ScientificExtension).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/scientific/json-schema/schema.json"),
-        );
-
-        // View Extension
-        assert!(obj.get_schema_url(SchemaType::ViewExtension).is_none());
-    }
-
-    #[test]
-    fn test_catalog_schema_urls() {
-        // v1.0.0-rc.1 Catalog
-        let obj = STACObject::new("1.0.0-rc.1", "Catalog");
-
-        // Core
-        assert_eq!(
-            obj.get_schema_url(SchemaType::Core).unwrap(),
-            String::from("https://schemas.stacspec.org/v1.0.0-rc.1/catalog-spec/json-schema/catalog.json"),
-        );
-
-        // EO Extension
-        assert!(obj.get_schema_url(SchemaType::EOExtension).is_none());
-
-        // Projection Extension
-        assert!(obj.get_schema_url(SchemaType::ProjectionExtension).is_none());
-
-        // Scientific Extension
-        assert!(obj.get_schema_url(SchemaType::ScientificExtension).is_none());
-
-        // View Extension
-        assert!(obj.get_schema_url(SchemaType::ViewExtension).is_none());
-    }
-
-    // #[test]
-    // fn test_get_schema() {
-    //
-    //
-    //     impl Object {
-    //         fn stac_version(&self) -> Version { Version::parse("1.0.0-rc.1").unwrap() }
-    //     }
-    //     impl Validate for Object {
-    //         fn get_type(&self) -> STACType { STACType::Catalog }
-    //         fn get_stac_version(&self) -> &Version { &self.stac_version() }
-    //     }
-    //     let obj = Object {};
-    //
-    //     let schema = obj.get_schema(SchemaType::Core).unwrap();
-    //
-    //     assert_eq!(schema["title"], "STAC Catalog Specification");
-    // }
-}
+// #[cfg(test)]
+// mod tests {
+//     use semver::Version;
+//     use super::*;
+//
+//     #[derive(Serialize)]
+//     struct STACObject {
+//         stac_version: Version,
+//         #[serde(rename = "type")]
+//         type_: String
+//     }
+//     impl STACObject {
+//         pub fn new(stac_version: &str, type_: &str) -> STACObject {
+//             let stac_version = Version::parse(stac_version).unwrap();
+//             let type_ = String::from(type_);
+//             STACObject {
+//                 stac_version,
+//                 type_
+//             }
+//         }
+//     }
+//     impl Validate for STACObject {
+//         fn get_type(&self) -> &String {
+//             &self.type_
+//         }
+//         fn get_stac_version(&self) -> &Version { &self.stac_version }
+//     }
+//
+//     #[test]
+//     fn test_item_schemas_urls() {
+//         // v1.0.0-rc.1 Item
+//         let obj = STACObject::new("1.0.0-rc.1", "Feature");
+//
+//         // Core
+//         assert_eq!(
+//             obj.get_schema_url("core").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/item-spec/json-schema/item.json"),
+//         );
+//
+//         // EO Extension
+//         assert_eq!(
+//             obj.get_schema_url("eo").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/eo/json-schema/schema.json"),
+//         );
+//
+//         // Projection Extension
+//         assert_eq!(
+//             obj.get_schema_url("projection").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/projection/json-schema/schema.json"),
+//         );
+//
+//         // Scientific Extension
+//         assert_eq!(
+//             obj.get_schema_url("scientific").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/scientific/json-schema/schema.json"),
+//         );
+//
+//         // View Extension
+//         assert_eq!(
+//             obj.get_schema_url("view").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/view/json-schema/schema.json"),
+//         );
+//     }
+//
+//     #[test]
+//     fn test_collection_schema_urls() {
+//         // v1.0.0-rc.1 Collection
+//         let obj = STACObject::new("1.0.0-rc.1", "Collection");
+//
+//         // Core
+//         assert_eq!(
+//             obj.get_schema_url("core").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/collection-spec/json-schema/collection.json"),
+//         );
+//
+//         // EO Extension
+//         assert!(obj.get_schema_url("eo").is_none());
+//
+//         // Projection Extension
+//         assert!(obj.get_schema_url("projection").is_none());
+//
+//         // Scientific Extension
+//         assert_eq!(
+//             obj.get_schema_url("scientific").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/extensions/scientific/json-schema/schema.json"),
+//         );
+//
+//         // View Extension
+//         assert!(obj.get_schema_url("view").is_none());
+//     }
+//
+//     #[test]
+//     fn test_catalog_schema_urls() {
+//         // v1.0.0-rc.1 Catalog
+//         let obj = STACObject::new("1.0.0-rc.1", "Catalog");
+//
+//         // Core
+//         assert_eq!(
+//             obj.get_schema_url("core").unwrap(),
+//             String::from("https://schemas.stacspec.org/v1.0.0-rc.1/catalog-spec/json-schema/catalog.json"),
+//         );
+//
+//         // EO Extension
+//         assert!(obj.get_schema_url("eo").is_none());
+//
+//         // Projection Extension
+//         assert!(obj.get_schema_url("projection").is_none());
+//
+//         // Scientific Extension
+//         assert!(obj.get_schema_url("scientific").is_none());
+//
+//         // View Extension
+//         assert!(obj.get_schema_url("view").is_none());
+//     }
+//
+//     #[test]
+//     fn test_get_schema() {
+//         let obj = STACObject::new("1.0.0-rc.1", "Catalog");
+//         let schema = obj.get_schema("core").unwrap();
+//
+//         assert_eq!(schema["title"], "STAC Catalog Specification");
+//     }
+// }
--- rustac-core/stac-validate/src/util.rs
+++ rustac-core/stac-validate/src/util.rs
@@ -1,42 +1,127 @@
+use reqwest::blocking::get;
 use semver::{VersionReq, Version};
+use serde_json::Value;
 
-use crate::SchemaType;
+use crate::error::{STACValidateResult, STACValidateError};
 
-pub fn root_url_from_version(version: &Version) -> String
+/// Gets the URL for the schema associated with the given STAC version, STAC object type, and schema
+/// type.
+///
+/// # Arguments
+///
+/// * `stac_version` - The STAC Spec version for the schema you want to fetch.
+/// * `stac_type` - The value of the `"type"` field for the STAC object you want to validate. This is
+///    used per the guidelines in [How to Differentiate STAC Files] to determine the type of the STAC
+///    object. This must be one of `"Catalog"`, `"Collection"`, or `"Feature"` (for Items).
+/// * `schema_type` - This must be either a STAC extension ID (e.g. `"eo"`) or the value `"core"`
+///    (to validate against the core spec).
+///
+/// # Errors
+///
+/// This function will return [`STACValidateError::Other`] if there is no schema URL associated with the given
+/// inputs.
+///
+/// [How to Differentiate STAC Files]: https://github.com/radiantearth/stac-spec/blob/master/best-practices.md#how-to-differentiate-stac-files
+pub fn get_schema_url(stac_version: &Version, stac_type: &str, schema_type: &str) -> STACValidateResult<String> {
+    let root_url = root_url_from_version(stac_version);
+    let path = path_from_stac_type(stac_type, schema_type);
+    let url = path.map(|path| {
+        format!("{}/{}", root_url.as_str(), path.as_str())
+    });
+    url.ok_or_else(|| STACValidateError::Other(String::from("Could not find schema URL.")))
+}
+
+/// Gets the JSON schema for the given STAC object and schema type as a
+/// [`Value`].
+///
+/// # Arguments
+///
+/// * `instance` - A [`Value`] representing the STAC object to validate
+/// * `schema_type` - This must be either a STAC extension ID (e.g. `"eo"`) or the value `"core"`
+///    (to validate against the core spec).
+///
+/// # Errors
+///
+/// In addition to the errors that may result from calling [`get_schema_url`], this function may
+/// return [`STACValidateError::JSONParse`] if there is an error parsing the schema from the JSON string.
+pub fn get_schema(instance: &Value, schema_type: &str) -> STACValidateResult<Value> {
+    let stac_version = instance.get("stac_version")
+        .ok_or_else(|| STACValidateError::Other(String::from("Could not get stac_version from instance.")))?
+        .as_str()
+        .ok_or_else(|| STACValidateError::Other(String::from("Could not get stac_version from instance.")))?;
+    let stac_version= Version::parse(stac_version)?;
+    let stac_type = instance.get("type")
+        .ok_or_else(|| STACValidateError::Other(String::from("Could not get type from instance.")))?
+        .as_str()
+        .ok_or_else(|| STACValidateError::Other(String::from("Could not get type from instance.")))?;
+    let schema_url = get_schema_url(&stac_version, stac_type, schema_type)?;
+
+    Ok(get(schema_url)?.json()?)
+}
+
+fn root_url_from_version(stac_version: &Version) -> String
 {
     let at_least_v1 = VersionReq::parse(">=1.0.0-beta.1").unwrap();
 
-    if at_least_v1.matches(&version) {
-        format!("https://schemas.stacspec.org/v{}", version.to_string())
+    if at_least_v1.matches(&stac_version) {
+        format!("https://schemas.stacspec.org/v{}", stac_version.to_string())
     } else {
-        format!("https://raw.githubusercontent.com/radiantearth/stac-spec/v{}", version.to_string())
+        format!("https://raw.githubusercontent.com/radiantearth/stac-spec/v{}", stac_version.to_string())
     }
 }
 
-pub fn path_from_stac_type(stac_type: &str, schema_type: &SchemaType) -> Option<String>
+fn path_from_stac_type(stac_type: &str, schema_type: &str) -> Option<String>
 {
     match schema_type {
-        SchemaType::Core => match stac_type {
+        "core" => match stac_type {
             "Feature" => Some(String::from("item-spec/json-schema/item.json")),
             "Collection" => Some(String::from("collection-spec/json-schema/collection.json")),
             "Catalog" => Some(String::from("catalog-spec/json-schema/catalog.json")),
             _ => None,
         },
-        SchemaType::EOExtension => match stac_type {
+        "eo" => match stac_type {
             "Feature" => Some(String::from("extensions/eo/json-schema/schema.json")),
             _ => None,
         },
-        SchemaType::ProjectionExtension => match stac_type {
+        "projection" => match stac_type {
             "Feature" => Some(String::from("extensions/projection/json-schema/schema.json")),
             _ => None,
         },
-        SchemaType::ScientificExtension => match stac_type {
+        "scientific" => match stac_type {
             "Feature" | "Collection" => Some(String::from("extensions/scientific/json-schema/schema.json")),
             _ => None,
         },
-        SchemaType::ViewExtension => match stac_type {
+        "view" => match stac_type {
             "Feature" => Some(String::from("extensions/view/json-schema/schema.json")),
             _ => None,
         },
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+    use super::*;
+
+    #[test]
+    fn test_get_item_core_schema_url() {
+        let version = Version::parse("1.0.0-rc.1").unwrap();
+        let url = get_schema_url(&version, "Feature", "core");
+
+        assert!(url.is_ok());
+        assert_eq!(url.unwrap(), "https://schemas.stacspec.org/v1.0.0-rc.1/item-spec/json-schema/item.json");
+    }
+
+    #[test]
+    fn test_get_item_core_schema() {
+        let instance = json!({
+            "type": "Feature",
+            "stac_version": "1.0.0-rc.1",
+        });
+        let schema = get_schema(&instance, "core");
+
+        assert!(schema.is_ok());
+        assert_eq!(schema.unwrap()["title"].as_str(), Some("STAC Item"));
     }
 }
