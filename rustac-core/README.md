73edc218c4e4042e8583c6ba745918cc3f276962
Jon Duckworth <duckontheweb@gmail.com>
2021-04-11 20:37:04 -0400

    Updates to main README and stac-core docs

--- rustac-core/README.md
+++ rustac-core/README.md
@@ -1,15 +1,13 @@
 # RuSTAC
 
-Rust(y) tools for working with the [SpatioTemporal Asset Catalog (STAC)] spec.
+Rust(y) tools for working with the [SpatioTemporal Asset Catalog (STAC)] spec. 
+
+**WARNING:** I am brand new to Rust (this is my first real Rust project). Any feedback on code structure and/or style is more than welcome.
 
 ## Crates
 
 * [`stac-core`](./stac-core) - Primitive types for working with STAC entities
 * [`stac-validate`](./stac-validate) - Tools for validating STAC entities using [`jsonschema`]
-
-## Caveat
-
-> This is my first Rust project. Feedback and input is more than welcome in the form of Issues and/or PRs!
 
 [SpatioTemporal Asset Catalog (STAC)]: https://stacspec.org/
 [`jsonschema`]: https://docs.rs/jsonschema
--- rustac-core/stac-core/src/lib.rs
+++ rustac-core/stac-core/src/lib.rs
@@ -8,13 +8,28 @@
 //! * [Collections](https://github.com/radiantearth/stac-spec/blob/master/collection-spec/collection-spec.md)
 //! * [Catalogs](https://github.com/radiantearth/stac-spec/blob/master/catalog-spec/catalog-spec.md)
 //! 
-//! All objects can be serialized from JSON using the [`serde`] crate.
-extern crate serde;
-extern crate serde_json;
-extern crate geojson;
-extern crate semver;
-extern crate chrono;
-
+//! For validation of STAC entities using JSON Schema, see the `stac-validate` crate.
+//! 
+//! # Data Types
+//! 
+//! All objects can be serialized from JSON using [`serde`] and [`serde_json`].
+//! 
+//! This library tries to be consistent in how it maps a STAC type to a Rust type. Any STAC attributes that are 
+//! required are represented as one of the Rust types listed below. Any optional STAC attributes 
+//! are represented as an [`Option`] with a value that is one of the Rust types listed below.
+//! 
+//! | STAC Type  | Rust Type             |
+//! |------------|-----------------------|
+//! | `string`   | [`String`]            |
+//! | `number`   | [`f32`]               |
+//! | `array`    | [`Vec`]               |
+//! | `datetime` | [`chrono::DateTime`] with [`chrono::FixedOffset`] |
+//! | `geometry` | [`geojson::Geometry`] |
+//! | `bbox`     | [`geojson::Bbox`]     |
+//! 
+//! STAC objects may contain fields not included in the STAC spec itself. These fields are not serialized into "primitive" 
+//! Rust types, but instead are flattened into a [`serde_json::Value`] in the `extra_fields` field on the `struct`.
+//! 
 pub use types::{
     catalog::Catalog,
     collection::{Collection, Extent, SpatialExtent, TemporalExtent},
